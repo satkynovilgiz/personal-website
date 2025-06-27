6099e3049c222b3cf438b50e6fbf1fb394cd186e
satkynovilgiz <satkynovilgiz008@gmail.com>
2025-06-27 18:22:59 +0600

    create text

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,5 +1,5 @@
+// node js for Sent for email
 import nodemailer from 'nodemailer';
-
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Method Not Allowed' });
