2923418c3b7e601bcc73f9ae06865146b048c6a1
satkynovilgiz <satkynovilgiz008@gmail.com>
2025-06-27 18:53:39 +0600

    fidee

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'ru'],
-    localeDetection: false,  // (опционально, если хочешь отключить автоопределение)
+    localeDetection: false,
   },
   defaultNS: 'common',
   ns: ['common'],
